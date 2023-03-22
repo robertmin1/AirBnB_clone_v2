c5d8e2886110a6c30f99eb17d11f21893e5fa76b
omolola taiwo <omololataiwo93@gmail.com>
2023-03-22 09:43:58 -0700

    upload file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,10 @@
--- Creating hbnb_dev_db database
--- Privileges for new user (hbnb_dev)
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Creating hbnb_test_db database
+-- Privileges for new user (hbnb_test)
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
+
