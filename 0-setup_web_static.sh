59e87907c0991763a23da9baf63e3ad0f8759e00
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-04-06 15:45:38 +0300

    Test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 # Script that configures Nginx server with some folders and files
-apt-get -y update
-apt-get -y install nginx
-service nginx start
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+# Install Nginx if it is not already installed
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+
+# Create necessary folders if they do not already exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo chown -R ubuntu:ubuntu /data/
+
+# Create a fake HTML file for testing purposes
+echo "<html>
+  <head>
+    <title>Testing Nginx configuration</title>
+  </head>
+  <body>
+    <p>This is a test page.</p>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link to the /data/web_static/releases/test/ folder
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Update the Nginx configuration file to serve /data/web_static/current/
 sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+
+# Restart Nginx
+sudo service nginx restart
