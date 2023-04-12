c2035faa4fbbc7271f779bc945ed8ca190a27ae7
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-04-12 22:57:06 +0300

    Test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ from fabric.api import local
 from datetime import datetime
 
 def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
     try:
         now = datetime.now().strftime("%Y%m%d%H%M%S")
         local("mkdir -p versions")
