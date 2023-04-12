4e287467a39b50f972255a69b7980ad4c52372a6
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-04-12 22:52:12 +0300

    Test

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    try:
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        archive_path = "versions/web_static_{}.tgz".format(now)
+        local("tar-cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
