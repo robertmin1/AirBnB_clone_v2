e18e69985e543b940ecdc876ac92f3704573a108
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-04-24 16:35:24 +0300

    Test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,18 +6,22 @@ starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
-    return "HBNB"
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def display(text):
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
