457c2ce865c0d1cfc146b63335081e517e8702cc
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-04-24 16:34:22 +0300

    Test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def display(text):
-    return "C {}".format(text)
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
