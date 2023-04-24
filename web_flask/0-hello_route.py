ed4fe10cfd97d30a8b9d455d8ba616b39661fdbc
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-04-24 15:52:09 +0300

    Test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,9 +7,8 @@ Routes:
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
